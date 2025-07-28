68a15b77e2414316f4e8f3863f304c19b3de575e
Sanlipta Das <127015991+sanlipta-das@users.noreply.github.com>
2025-07-28 12:34:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+// Dark / light mode
+
+const btn = document.getElementById("modeToggle");
+const btn2 = document.getElementById("modeToggle2");
+const themeIcons = document.querySelectorAll(".icon");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme === "dark") {
+  setDarkMode();
+}
+
+btn.addEventListener("click", function () {
+  setTheme();
+});
+
+btn2.addEventListener("click", function () {
+  setTheme();
+});
+
+function setTheme() {
+  let currentTheme = document.body.getAttribute("theme");
+
+  if (currentTheme === "dark") {
+    setLightMode();
+  } 
+  else {
+    setDarkMode();
+  }
+}
+
+function setDarkMode() {
+  document.body.setAttribute("theme", "dark");
+  localStorage.setItem("theme", "dark");
+
+  themeIcons.forEach((icon) => {
+    icon.src = icon.getAttribute("src-dark");
+  });
+}
+
+function setLightMode() {
+  document.body.removeAttribute("theme");
+  localStorage.setItem("theme", "light");
+
+  themeIcons.forEach((icon) => {
+    icon.src = icon.getAttribute("src-light");
+  });
+}
+
+const words = document.querySelectorAll('.section__text__p2 .word');
+  let currentIndex = 0;
+
+  setInterval(() => {
+    words[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % words.length;
+    words[currentIndex].classList.add('active');
+  }, 2000); // 2000ms = 2 seconds per word
